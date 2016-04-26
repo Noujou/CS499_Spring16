98a8ef57c5bd40d7b643a9c311c54651ab6c8109
George Bencivengo <gbencivengo4@gmail.com>
2016-04-25 23:45:58 -0400

    added TPS shooter and Zombie Pokemon

---
+++ ZombiePokemon/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    bool isDead;
+    bool damaged;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void TakeDamage(int damage)
+    {
+        damaged = true;
+
+        currentHealth -= damage;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+    }
+}
---
+++ ZombiePokemon/Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+    public int shotDamage = 1;
+    //public Transform objToMove;
+	// Use this for initialization
+	void Start () {
+        Cardboard.SDK.OnTrigger += On_Click;
+	}
+    void On_Click() {
+        //Debug.Log("triggered");
+        int shootableMask = LayerMask.GetMask("Shootable");
+        Ray shootRay = GetComponentInChildren<CardboardHead>().Gaze;
+        Debug.DrawRay(shootRay.origin, shootRay.direction, Color.blue, 1);
+        RaycastHit shootHit;
+        if (Physics.Raycast(shootRay, out shootHit, 250, shootableMask))
+        {
+            Debug.Log("PIKA");
+            PokemonHealth currentPokeHP = shootHit.collider.GetComponent<PokemonHealth>(); //get the HP of the pokemon that you just hit
+            currentPokeHP.TakeDamage(shotDamage); //subtract the pokemon's HP from the damage of the shot
+            Debug.Log("current pokemon's hp is " + currentPokeHP.currentHealth);
+        }
+        else
+            Debug.Log("Miss");
+    }
+    void Update()
+    {
+        Cardboard.SDK.UpdateState();
+        Pose3D head = Cardboard.SDK.HeadPose;
+        Ray r = GetComponentInChildren<CardboardHead>().Gaze;
+        Debug.DrawRay(r.origin, r.direction, Color.blue, 1);
+
+    }
+}
